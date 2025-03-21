4a3facb6a6b1195ba648fb347e049eb38e08f46c
moazzam <moazzammaan420@gmail.com>
2025-03-21 12:58:25 -0400

    feat: added navmesh and fixed plane coordinate

--- public/components/plane.js
+++ public/components/plane.js
@@ -1,28 +1,39 @@
 //ADD PRE AND POST CONDITIONS
+/*
+elevation.txt format:
+  line 1: (integer) n = number of segments to be build in plane
+  must be followed by 6n^2 lines, where each line contains the 3 floats [a,b,c] (coordinates).
+  Let: 
+  "North” = -Z
+  “South” = +Z
+  “East” = +X
+  “West” = -X
+  “Up” = +Y
+  “Down” = -Y
+  where the XYZ orientation is based off the AFRAME coordinate system. The camera initially faces north (-Z) 
+  when the scene begins.
+  a: -ve is west and +ve is east
+  b: -ve is north and +ve is south
+  c: -ve is up and +ve is down
+
+  6 vertices form a rectangle
+  
+  1, 5 are the standalone vertices
+  2 = 4
+  3 = 6
+
+*/
 AFRAME.registerComponent("custom-plane-builder", {
   schema: {
     filename: { type: "string", default: "inputdata.txt" },
-    elevation: { type: "boolean", default: false },
   },
 
   init: async function () {
-    //if elevation is true, attach the listener now (before setAttribute).
-    if (this.data.elevation) {
-      this.el.addEventListener(
-        "componentchanged",
-        (evt) => {
-          if (evt.detail.name === "geometry") {
-            this.modifyVertices(data);
-          }
-        },
-        { once: true }
-      );
-    }
-
+   
     // Fetch the file contents.
     const response = await fetch(`tests/${this.data.filename}`);
     let data = await response.text();
-
+    
     const firstNewlineIdx = data.indexOf("\n");
     if (firstNewlineIdx === -1) {
       console.error("File does not contain a newline; cannot parse segments!");
@@ -40,52 +51,86 @@ AFRAME.registerComponent("custom-plane-builder", {
 
     // remove the first line from data
     data = data.substring(firstNewlineIdx + 1);
+    this.remainingElevationData = data;
 
     this.el.setAttribute("geometry", {
       primitive: "plane",
       segmentsHeight: segments,
       segmentsWidth: segments,
     });
-  },
 
+    this.segmentsReady = true;
+    this.el.emit('segments-ready');
+  },
+});
+
+AFRAME.registerComponent("add-elevation", {
+  schema: {
+    elevation: { type: "boolean", default: false },
+  },
+  init: function () {
+    const el = this.el;
+  
+    if (el.components['custom-plane-builder'] && el.components['custom-plane-builder'].segmentsReady) {
+      this.modifyVertices();
+    } else {
+      
+      el.addEventListener('segments-ready', () => this.modifyVertices());
+      console.log("we did dis");
+    }
+  },
+  
   modifyVertices: function (data) {
     const mesh = this.el.getObject3D("mesh");
     if (!mesh) {
+      console.error("Mesh not found")
       return;
     }
 
     const positionAttr = mesh.geometry.attributes.position;
     // console.log(positionAttr);
     if (!positionAttr) {
+      console.error("positionAttr not found")
       return;
     }
 
     const groupSize = this.el.getAttribute("geometry").segmentsHeight;
+    const elevationData1 = this.el.components['custom-plane-builder'].remainingElevationData.split("\n");
 
-    const elevationData1 = data.split("\n");
     if (elevationData1.length !== positionAttr.count) {
       console.error(`Incorrect number of vertices in file`);
       return;
     }
+    
+    this.el.object3D.parent.updateMatrixWorld(true);
+    mesh.updateMatrixWorld(true); //to make sure worldtolocal works
+
 
     let newVertices = new Float32Array(positionAttr.array.length);
     let count2 = 0;
-
+    let tempV3 = new THREE.Vector3();
     for (let i = 0; i < elevationData1.length; i++) {
       const [xStr, yStr, zStr] = elevationData1[i].split(/\s+/);
-      newVertices[count2] = parseFloat(xStr);
+      tempV3.set(parseFloat(xStr),parseFloat(yStr),parseFloat(zStr));
+      console.log(tempV3);
+      mesh.updateMatrixWorld(true);
+      tempV3 = mesh.worldToLocal(tempV3);
+      console.log(tempV3);
+      newVertices[count2] = tempV3.x;
       count2++;
-      newVertices[count2] = parseFloat(yStr);
+      newVertices[count2] = tempV3.y;
       count2++;
-      newVertices[count2] = parseFloat(zStr);
+      newVertices[count2] = tempV3.z;
       count2++;
-    }
+    } 
 
+    const newVerticesBuffer = new THREE.BufferAttribute(newVertices, 3)
     mesh.geometry.setAttribute(
       "position",
-      new THREE.BufferAttribute(newVertices, 3)
+      newVerticesBuffer
     );
+  
     positionAttr.needsUpdate = true;
     mesh.geometry.computeVertexNormals();
   },
-});
+  });
---
+++ public/components/simple-navmesh-constraint.js
@@ -0,0 +1,207 @@
+/* global AFRAME, THREE */
+
+AFRAME.registerComponent('camera-boost', {
+  schema: {
+    acceleration: { default: 50 }
+  },
+
+  init: function () {
+    // Use the value from the schema
+    const accel = this.data.acceleration;
+    // Set the wasd-controls attribute
+    this.el.setAttribute('wasd-controls', `acceleration: ${accel}`);
+
+  }
+});
+
+AFRAME.registerComponent('simple-navmesh-constraint', {
+  schema: {
+    enabled: {
+      default: true
+    },
+    navmesh: {
+      default: ''
+    },
+    fall: {
+      default: 0.5
+    },
+    height: {
+      default: 1.6
+    },
+    exclude: {
+      default: ''
+    },
+    xzOrigin: {
+      default: ''
+    }
+  },
+
+  init: function () {
+    this.onSceneUpdated = this.onSceneUpdated.bind(this);
+
+    this.el.sceneEl.addEventListener('child-attached', this.onSceneUpdated);
+    this.el.sceneEl.addEventListener('child-detached', this.onSceneUpdated);
+
+    this.objects = [];
+    this.excludes = [];
+  },
+
+  remove: function () {
+    this.el.sceneEl.removeEventListener('child-attached', this.onSceneUpdated);
+    this.el.sceneEl.removeEventListener('child-detached', this.onSceneUpdated);
+  },
+
+  onSceneUpdated: function (evt) {
+    // We already have an update on the way
+    if (this.entitiesChanged) { return; }
+  
+    const navmeshSelector = this.data.navmesh;
+    const excludeSelector = this.data.exclude;
+  
+    // If both are empty, just return.
+    if (!navmeshSelector && !excludeSelector) { return; }
+  
+    // If we have a navmesh selector, check it
+    if (navmeshSelector && navmeshSelector.length > 0 && evt.detail.el.matches(navmeshSelector)) {
+      this.entitiesChanged = true;
+    }
+  
+    // If we have an exclude selector, check it
+    if (excludeSelector && excludeSelector.length > 0 && evt.detail.el.matches(excludeSelector)) {
+      this.entitiesChanged = true;
+    }
+  },
+  
+
+  updateNavmeshEntities: function () {
+    this.objects.length = 0;
+    this.excludes.length = 0;
+  
+    // 1) Instead of using this.data.navmesh, we use #segmented-plane
+    const planeEl = document.querySelector('#segmented-plane');
+    if (!planeEl) {
+      console.warn('simple-navmesh-constraint: #segmented-plane not found in DOM!');
+    } else {
+      // 2) Push its THREE.Object3D into this.objects
+      this.objects.push(planeEl.object3D);
+    }
+  
+    // dont need this for now, but eh
+    if (this.data.exclude.length > 0) {
+      for (const excluded of document.querySelectorAll(this.data.exclude)) {
+        this.objects.push(excluded.object3D);
+        this.excludes.push(excluded);
+      }
+    }
+  
+    this.entitiesChanged = false;
+  },
+  
+
+  update: function () {
+    this.lastPosition = null;
+    this.xzOrigin = this.data.xzOrigin ? this.el.querySelector(this.data.xzOrigin) : this.el;
+
+    this.updateNavmeshEntities();
+  },
+
+  tick: (function () {
+    const nextPosition = new THREE.Vector3();
+    const tempVec = new THREE.Vector3();
+    const scanPattern = [
+      [0,1], // Default the next location
+      [0,0.5], // Check that the path to that location was fine
+      [30,0.4], // A little to the side shorter range
+      [-30,0.4], // A little to the side shorter range
+      [60,0.2], // Moderately to the side short range
+      [-60,0.2], // Moderately to the side short range
+      [80,0.06], // Perpendicular very short range
+      [-80,0.06], // Perpendicular very short range
+    ];
+    const down = new THREE.Vector3(0,-1,0);
+    const raycaster = new THREE.Raycaster();
+    const gravity = -1;
+    const maxYVelocity = 0.5;
+    const results = [];
+    let yVel = 0;
+    let firstTry = true;
+    
+    return function tick(time, delta) {
+      if (this.data.enabled === false) return;
+      if (this.entitiesChanged) {
+        this.updateNavmeshEntities();
+      }
+      if (this.lastPosition === null) {
+        firstTry = true;
+        this.lastPosition = new THREE.Vector3();
+        this.xzOrigin.object3D.getWorldPosition(this.lastPosition);
+        if (this.data.xzOrigin) this.lastPosition.y -= this.xzOrigin.object3D.position.y;
+      }
+      
+      const el = this.el;
+      if (this.objects.length === 0) return;
+
+      this.xzOrigin.object3D.getWorldPosition(nextPosition);
+      if (this.data.xzOrigin) nextPosition.y -= this.xzOrigin.object3D.position.y;
+      if (nextPosition.distanceTo(this.lastPosition) <= 0.01) return;
+      
+      let didHit = false;
+      // So that it does not get stuck it takes as few samples around the user and finds the most appropriate
+      scanPatternLoop:
+      for (const [angle, distance] of scanPattern) {
+        tempVec.subVectors(nextPosition, this.lastPosition);
+        tempVec.applyAxisAngle(down, angle*Math.PI/180);
+        tempVec.multiplyScalar(distance);
+        tempVec.add(this.lastPosition);
+        tempVec.y += maxYVelocity;
+        tempVec.y -= this.data.height;
+        raycaster.set(tempVec, down);
+        raycaster.far = this.data.fall > 0 ? this.data.fall + maxYVelocity : Infinity;
+        raycaster.intersectObjects(this.objects, true, results);
+        
+        if (results.length) {
+          // If it hit something we want to avoid then ignore it and stop looking
+          for (const result of results) {
+            if(this.excludes.includes(result.object.el)) {
+              results.splice(0);
+              continue scanPatternLoop;
+            }
+          }
+          const hitPos = results[0].point;
+          results.splice(0);
+          hitPos.y += this.data.height;
+
+          // ========
+          //  GRAVITY 
+          // ========
+          // if (nextPosition.y - (hitPos.y - yVel*2) > 0.01) {
+          //   yVel += Math.max(gravity * delta * 0.001, -maxYVelocity);
+          //   hitPos.y = nextPosition.y + yVel;
+          // } else {
+          //   yVel = 0;
+          // }
+          
+          tempVec.copy(hitPos);
+          this.xzOrigin.object3D.parent.worldToLocal(tempVec);
+          tempVec.sub(this.xzOrigin.object3D.position);
+          if (this.data.xzOrigin) tempVec.y += this.xzOrigin.object3D.position.y;
+          this.el.object3D.position.add(tempVec);
+          
+          this.lastPosition.copy(hitPos);
+          didHit = true;
+          break;
+        }
+        
+      }
+      
+      if (didHit) {
+        firstTry = false;
+      }
+      
+      if (!firstTry && !didHit) {
+        this.el.object3D.position.copy(this.lastPosition);
+        this.el.object3D.parent.worldToLocal(this.el.object3D.position);
+      }
+    }
+  }())
+});
